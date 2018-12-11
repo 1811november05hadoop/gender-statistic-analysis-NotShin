5fcfbe0da59b4c011ebacc72cb5d2c600b15084e
NotShin <alexmenteer10@live.com>
2018-12-10 20:00:01 -0800

    Add empty class files for BQ2

---
+++ GenderStatisticAnalysis/src/main/java/com/revature/FEducationAverageIncreaseUS2000.java
@@ -0,0 +1,8 @@
+package com.revature;
+
+public class FEducationAverageIncreaseUS2000 {
+	
+	public static void main(String[] args) {
+		
+	}
+}
---
+++ GenderStatisticAnalysis/src/main/java/com/revature/reduce/FEducationAverageIncreaseUS2000Reducer.java
@@ -0,0 +1,5 @@
+package com.revature.reduce;
+
+public class FEducationAverageIncreaseUS2000Reducer {
+	
+}
