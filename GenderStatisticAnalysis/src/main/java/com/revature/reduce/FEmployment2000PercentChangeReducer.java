842618958e313c4d7f4066f8ba0137651e666e18
NotShin <alexmenteer10@live.com>
2018-12-12 16:23:04 -0800

    Remove reducer classes for BQ3 and BQ4

--- GenderStatisticAnalysis/src/main/java/com/revature/reduce/FEmployment2000PercentChangeReducer.java
+++
@@ -1,5 +0,0 @@
-package com.revature.reduce;
-
-public class FEmployment2000PercentChangeReducer {
-
-}
--- GenderStatisticAnalysis/src/main/java/com/revature/reduce/MEmployment2000PercentChangeReducer.java
+++
@@ -1,5 +0,0 @@
-package com.revature.reduce;
-
-public class MEmployment2000PercentChangeReducer {
-
-}
